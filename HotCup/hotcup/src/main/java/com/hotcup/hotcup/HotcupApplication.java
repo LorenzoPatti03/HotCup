ffbd1b980ccc5c2fd1b3da346754d901b444a2db
LorenzoPatti03 <Lorenzo.pattipersonal@gmail.com>
2025-10-24 15:34:34 +0200

    ok

---
+++ HotCup/hotcup/src/main/java/com/hotcup/hotcup/HotcupApplication.java
@@ -0,0 +1,13 @@
+package com.hotcup.hotcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotcupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotcupApplication.class, args);
+	}
+
+}
---
+++ HotCup/hotcup/src/test/java/com/hotcup/hotcup/HotcupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotcup.hotcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotcupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
